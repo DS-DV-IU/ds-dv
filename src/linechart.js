5234809b090454af13e17dfb578e39a33a27cf82
Nguyen Vo <dashzero123@gmail.com>
2023-05-29 20:16:32 +0700

    added a dashboard template, resized the line-chart

--- src/linechart.js
+++ src/linechart.js
@@ -17,7 +17,7 @@ d3.csv("https://raw.githubusercontent.com/DS-DV-IU/ds-dv/main/data/linechart.csv
 
             // Set the margins
             var margin = { top: 60, right: 100, bottom: 20, left: 80 },
-                width = 1900 - margin.left - margin.right,
+                width = 1450 - margin.left - margin.right,
                 height = 600 - margin.top - margin.bottom;
             // Create the svg canvas 
             var svg = d3.select("body")
